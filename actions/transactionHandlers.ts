4f18116b4dbe6ef3d59284b26746ecda721aef0c
Wilson Cusack <cusackw@gmail.com>
2022-03-04 13:18:44 -0500

    added to filter, seems contract does nothing to filter if no events or calls are filtered

--- actions/transactionHandlers.ts
+++ actions/transactionHandlers.ts
@@ -23,10 +23,16 @@ export const defaultHandleTransaction: ActionFn = async (context: Context, event
 	// handling unlikely case where someone just sends ETH to the contract
 	// or there is a transaction with no event logs
 	for (const log of transactionEvent.logs) {
-		const event = iface.parseLog(log)
-		if(IS_INTERESTING_EVENT[event.name]){
-			// notify 
-			pushEvent(event.name, transactionEvent.hash)
+		try{
+			const event = iface.parseLog(log)
+			if(IS_INTERESTING_EVENT[event.name]){
+				// notify 
+				pushEvent(event.name, transactionEvent.hash)
+			}
+		} catch(err) {
+			// likely there is an event, e.g. Transfer
+			// which is not in our ABI
+			console.log(err)
 		}
 	}
 }
