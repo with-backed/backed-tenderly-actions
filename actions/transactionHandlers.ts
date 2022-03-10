deff7f364a0dd6e74dd305971a270c254a2cf639
Adam Gobran <adam.gobran@coinbase.com>
2022-03-10 11:15:09 -0500

    nft-67: push messages to SQS off on-chain events

--- actions/transactionHandlers.ts
+++ actions/transactionHandlers.ts
@@ -5,10 +5,11 @@ import {
 	TransactionEvent,
 } from '@tenderly/actions'
 import { ethers } from 'ethers';
+import { SQS } from 'aws-sdk';
 
 import { abi } from './abis/NFTLoanFacilitator';
 
-const IS_INTERESTING_EVENT : { [key: string]: boolean } = {
+const IS_INTERESTING_EVENT: { [key: string]: boolean } = {
 	"CreateLoan": true,
 	"Repay": true,
 	"SeizeCollateral": true,
@@ -23,13 +24,13 @@ export const defaultHandleTransaction: ActionFn = async (context: Context, event
 	// handling unlikely case where someone just sends ETH to the contract
 	// or there is a transaction with no event logs
 	for (const log of transactionEvent.logs) {
-		try{
+		try {
 			const event = iface.parseLog(log)
-			if(IS_INTERESTING_EVENT[event.name]){
+			if (IS_INTERESTING_EVENT[event.name]) {
 				// notify 
 				pushEvent(event.name, transactionEvent.hash)
 			}
-		} catch(err) {
+		} catch (err) {
 			// likely there is an event, e.g. Transfer
 			// which is not in our ABI
 			console.log(err)
@@ -37,12 +38,27 @@ export const defaultHandleTransaction: ActionFn = async (context: Context, event
 	}
 }
 
-const pushEvent = (eventName: string, transactionHash: string) => {
+const pushEvent = (eventName: string, txHash: string) => {
 	// TODO
+	const sqs = new SQS({
+		region: 'us-east-1',
+		credentials: {
+			accessKeyId: process.env.AWS_ACCESS_KEY!,
+			secretAccessKey: process.env.AWS_SECRET_KEY!,
+		},
+	})
+	sqs.sendMessage({
+		QueueUrl: process.env.EVENTS_SQS_URL!,
+		MessageBody: JSON.stringify({
+			eventName,
+			txHash
+		})
+	})
+
 	console.log(
 		`
 		eventName: ${eventName},
-		transactionHash: ${transactionHash}
+		transactionHash: ${txHash}
 		`
 	)
 }
